f7d692b88db16e6a1c1c16cae2741f2fd94ddd68
Yasmin Behi <ybehi0001@yahoo.com>
2019-04-17 14:02:48 -0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("********\n*\n*******\n*\n********");
+    }
+}
